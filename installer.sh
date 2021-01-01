5bfe412d39e9737e9f97208456ec3fa502fba47f
Andres Galvan <andresgalfajar@gmail.com>
2021-01-01 16:54:46 -0500

    Check if path already exist on installation

--- installer.sh
+++ installer.sh
@@ -3,6 +3,13 @@
 mkdir -p ~/.config/create-app
 wget https://raw.githubusercontent.com/afgalvan/create-app/web/create_app.sh
 chmod +x create_app.sh
+if [ -d "$1"]
+    echo "yarn" > ~/.config/create-app/default_pm
+else
+    echo "$1" > ~/.config/create-app/default_pm
+fi
+if [ -d ~/.config/create-app/create_app.sh ]; then
+    rm -f ~/.config/create-app/create_app.sh
+fi
 mv create_app.sh ~/.config/create-app/
-echo "alias create-app=$HOME/.config/create-appcreate_app.sh" >> ~/.bashrc
-# echo "alias create-app=$HOME/Documents/Programming/Projects/typescript/create-app/create_app.sh" >> test.sh
+echo "alias create-app=$HOME/.config/create-app/create_app.sh" >> ~/.bashrc
