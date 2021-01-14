7145c11417594f4929ba710e25854c720c6533b3
Andres Galvan <andresgalfajar@gmail.com>
2021-01-14 15:20:26 -0500

    Add basic test samples

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('App template');
+export class Numbers {
+  public isOdd(numbers: number[]): boolean[] {
+    const answer = numbers.map((value: number) => {
+      if (value % 2 == 0) return false;
+      return true;
+    });
+    return answer;
+  }
+}
---
+++ tests/src/index.test.ts
@@ -0,0 +1,23 @@
+import { Numbers } from '../../src/index';
+
+describe('Numbers', () => {
+  describe('isOdd', () => {
+    // eslint-disable-next-line quotes
+    it("Should return an array and his value must be true if it's odd", () => {
+      const numbers = new Numbers();
+      const expectedArray = [true, false, true, false, false, true, true];
+      const subject = [3, 2, 9, 4, 6, 5, 23];
+
+      expect(numbers.isOdd(subject)).toStrictEqual(expectedArray);
+    });
+  });
+});
+
+test('Numbers', async () => {
+  const numbers = new Numbers();
+  const subject = [3, 2, 9, 4, 6, 5, 23];
+  const expectedArray = [true, false, true, false, false, true, true];
+  const data = numbers.isOdd(subject);
+
+  expect(data).toStrictEqual(expectedArray);
+});
