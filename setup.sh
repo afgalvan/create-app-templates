fe0409d97502bc28a48023e335b476dfa3fb0990
Andrés Galván <62343874+afgalvan@users.noreply.github.com>
2021-01-18 14:12:14 -0500

    Prevent errors in the setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-sudo apt update
-sudo apt install make
-make init
-rm -f setup.sh
+{
+    make init
+} && {
+    rm -f setup.sh
+    exit 0
+} || {
+    echo -e "e\[31mError. Make sure to have \"make\" installed.e\[0m"
+    echot "Install it and try: bash setup.sh"
+    exit 1
+}
