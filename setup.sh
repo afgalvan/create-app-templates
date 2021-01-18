d40e29c2f9022c848ac5fc32545e3341638d01f4
Andrés Galván <62343874+afgalvan@users.noreply.github.com>
2021-01-18 14:21:49 -0500

    fix typo echot to echo.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@
     exit 0
 } || {
     echo -e "e\[31mError. Make sure to have \"make\" installed.e\[0m"
-    echot "Install it and try: bash setup.sh"
+    echo "Install it and try: bash setup.sh"
     exit 1
 }
