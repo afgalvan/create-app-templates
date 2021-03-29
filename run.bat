80207c2cd99dcf9474964aeb9ab018ebf576df98
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-29 16:17:25 -0500

    Replace runners with yarn scripts

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-
-cd build/classes
-java edu.unicesar.path.to.Main
--- run.ps1
+++
@@ -1,8 +0,0 @@
-function exec() {
-    Clear-Host
-    Set-Location ./out/production/actividad3
-    java edu.unicesar.taller.main.Main
-    Set-Location ../../../
-}
-
-exec
