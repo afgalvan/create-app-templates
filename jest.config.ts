54ccb3209ec55f9c5a52c6f287c892a0941fe5b8
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-31 12:24:21 -0500

    Use ts as config file for jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  name: 'template',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
