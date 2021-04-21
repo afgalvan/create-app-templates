ed7678e5042477703edf11121ba5845e47f511ad
Andres Galvan <afgalvan@unicesar.edu.co>
2021-04-21 13:41:12 -0500

    Update junit and use lombok

--- src/main/java/co/edu/unicesar/purpose/main/Main.java
+++ src/main/java/co/edu/unicesar/purpose/main/Main.java
@@ -1,8 +1,11 @@
 package co.edu.unicesar.purpose.main;
 
+import lombok.val;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello template!");
+        val message = "Hello template!";
+        System.out.println(message);
     }
 }
---
+++ src/test/java/co/edu/unicesar/purpose/main/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package co.edu.unicesar.purpose.main;
+
+/*
+ * Test example
+ */
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleUnitTest {
+    @Test
+    void addsTwoNumbers() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+}
