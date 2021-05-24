68b698b550c1d8ea68a304e7346bb9bceb2342b4
Andres Galvan <afgalvan@unicesar.edu.co>
2021-05-24 15:27:07 -0500

    Ignore js files

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
-
 export default {
   name: 'template',
   preset: 'ts-jest',
--- src/app/about.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../assets/scss/normalize.css");
-console.log('about');
