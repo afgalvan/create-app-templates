ae9bd53f667c5cef91b8a5f784742fdf4d595e45
Andres Galvan <afgalvan@unicesar.edu.co>
2021-07-21 16:00:39 -0500

    Change test root file

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
-export default {
-  name: 'template',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
