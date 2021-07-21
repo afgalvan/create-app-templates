6f1d5419b0f1d4ec905f33d0241d1a34b4adac31
Andres Galvan <afgalvan@unicesar.edu.co>
2021-07-21 15:57:30 -0500

    Change Dockerfile on npm choose

--- setup.sh
+++ setup.sh
@@ -2,4 +2,24 @@
 
 package_manager="$1"
 # "$package_manager" install --silent
+
+if [ "$package_manager" == "npm" ]; then
+    echo "FROM node:14.17.0-alpine3.13
+
+WORKDIR /app
+
+# Add package.lock.json file
+COPY ./package.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 4000
+
+RUN npm build
+
+CMD [ \"npm\", \"start\" ]" > Dockerfile
+fi
+
 rm -f setup.sh
